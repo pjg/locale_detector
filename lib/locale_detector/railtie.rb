dd058245719362b77379756a81d883851f8fe266
Paweł Gościcki <pawel.goscicki@gmail.com>
2011-09-16 16:00:53 +0200

    Fix typo in initializer description

--- lib/locale_detector/railtie.rb
+++ lib/locale_detector/railtie.rb
@@ -3,7 +3,7 @@ require 'rails'
 
 module LocaleDetector
   class Railtie < Rails::Railtie
-    initializer "locale_detector.prepend_before_filter" do
+    initializer "locale_detector.append_before_filter" do
       ActiveSupport.on_load(:action_controller) do
         ActionController::Base.send(:include, LocaleDetector::Filter)
       end
